a0e5c8fba32f63816ffe4e1f59de452daf3d5c01
engineerdogan <99654850+engineerdogan@users.noreply.github.com>
2024-09-02 16:07:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-document.getElementById("macInput").addEventListener("input", function (e) {
-    let value = e.target.value;
-
-    // Tüm ayırıcı karakterleri kaldır
+function formatMacAddress(value) {
     value = value.replace(/[^0-9A-Fa-f]/g, '');
+    return value.length > 2 ? value.match(/.{1,2}/g).join('-').toUpperCase() : value.toUpperCase();
+}
 
-    // 2'li gruplar halinde ayırıcı karakterleri ekle
-    if (value.length > 2) {
-        value = value.match(/.{1,2}/g).join('-');
-    }
+function isValidMacAddress(macAddress) {
+    const macRegex = /^([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2}$/;
+    return macRegex.test(macAddress);
+}
 
-    // MAC adresini büyük harfe çevir
-    e.target.value = value.toUpperCase();
+document.getElementById("macInput").addEventListener("input", function (e) {
+    e.target.value = formatMacAddress(e.target.value);
+    document.getElementById("generateButton").disabled = e.target.value.length === 0;
 });
 
 document.getElementById("generateButton").addEventListener("click", function () {
     let macAddress = document.getElementById("macInput").value.trim().toUpperCase();
-    
-    // Geçerli MAC adresi kontrolü
-    const macRegex = /^([0-9A-Fa-f]{2}[-:]){5}[0-9A-Fa-f]{2}$/;
-    
-    if (macRegex.test(macAddress)) {
-        // MAC adresinin SHA-256 hash'ini hesapla
+    let resultElement = document.getElementById("result");
+
+    if (isValidMacAddress(macAddress)) {
         const hash = CryptoJS.SHA256(macAddress).toString(CryptoJS.enc.Base64);
-        
-        // Base64 hash'inden ilk 12 karakteri al
         let password = hash.substring(0, 12);
-        
-        // Şifreyi sadece alfanümerik ve özel karakterler ile sınırla
-        password = password.replace(/[^a-zA-Z0-9!@#$%^&*()_+]/g, '');
-
-        // Eğer şifre 12 karakterden kısa ise tamamla
-        if (password.length < 12) {
-            password = password.padEnd(12, '0');
-        }
-
-        // Sonuç alanını güncelle
-        let resultElement = document.getElementById("result");
         resultElement.textContent = password;
+        resultElement.style.color = "#333";
+        document.getElementById("macInput").style.borderColor = "#ddd";
     } else {
-        document.getElementById("result").textContent = "Lütfen geçerli bir MAC adresi girin.";
+        resultElement.textContent = "Lütfen geçerli bir MAC adresi girin.";
+        resultElement.style.color = "red";
+        document.getElementById("macInput").style.borderColor = "red";
     }
-});
-
-document.getElementById("macInput").addEventListener("input", function () {
-    document.getElementById("generateButton").disabled = false; // Butonu etkinleştir
 });
